12446d6e59832b30b847f2d1be328469d879b33c
QuentinDll <quentin.delplace13@gmail.com>
2018-08-24 09:49:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+// declaration du tableaux
+$months = array (
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "aout",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre"
+);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Exercice 2</title>
+    </head>
+    <body>
+        <p>
+    <?php
+    // affichage de la troisiéme valeur du tableaux
+    echo $months[2];
+    ?>
+        </p>
+    </body>
+</html>
